bbb192f77ece46941e66b72339e84dba4977e1e7
nosun <nosun2008@126.com>
2020-06-24 17:16:00 +0800

    update

--- config/api-log.php
+++ config/api-log.php
@@ -2,4 +2,6 @@
 
 return [
     'enable' => env('API_LOG_ENABLE','false'),
+    'execute_time' => env('API_EXECUTE_TIME','false'),
+    'delimiter' => env('API_DELIMITER',' '),
 ];
--- src/ApiLog.php
+++ src/ApiLog.php
@@ -7,6 +7,9 @@ use Log;
 
 class ApiLog
 {
+
+    protected $startTime;
+
     /**
      * Handle an incoming request.
      *
@@ -17,13 +20,43 @@ class ApiLog
     public function handle($request, Closure $next)
     {
 
-        $response = $next($request);
-
-        if (config('api-log.enable')) {
-            // 需要提前建立 api-debug channel
-            Log::channel('api')->info(json_encode($response));
+        if(!defined('LARAVEL_START')){
+            define('LARAVEL_START',microtime(true));
         }
 
-        return $response;
+        $response = $next($request);
+
+        $request->header('accept','application/json');
+
+        if (config('api-log.enable') == true) {
+
+            $delimiter = config('api-log.delimiter');
+
+            $req = [
+                'url' => $request->url(),
+                'header' => $request->header(),
+                'body' => $request->post(),
+            ];
+
+            $data = [
+                'request' => $req,
+                'response' => $response,
+            ];
+
+            $executionTime = 0;
+
+            if (config('api-log.execute_time') == true) {
+                $executionTime = floor(1000000 * (microtime(true) - LARAVEL_START));
+            }
+
+            $message = implode($delimiter, [
+                $request->getClientIp(),
+                $executionTime,
+                json_encode($data)
+            ]);
+
+            Log::channel('api')->info($message);
+        }
+
     }
 }
